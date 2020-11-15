4bea9fad93c1431855039017421b37870e482ccd
Tikhon Izmailov <fisioterapevt@gmail.com>
2020-11-15 19:46:20 +0600

    added HomeScreen, components:  Product, Rating; installed dependencies: react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import HomeScreen from "./screens/HomeScreen/HomeScreen";
 
 const App = () => {
-	return <div className="App"></div>;
+	return (
+		<>
+			<Header />
+			<main className="py-3">
+				<Container>
+					<HomeScreen />
+				</Container>
+			</main>
+			<Footer />
+		</>
+	);
 };
 
 export default App;
