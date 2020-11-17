4b71d2a1a375ffcd5fdee7e7cb3f4b4de966302a
Tikhon Izmailov <fisioterapevt@gmail.com>
2020-11-17 16:06:26 +0600

    added routes in productRoutes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,14 +3,30 @@ const Product = require("../models/productModel");
 
 const router = express.Router();
 
+//* @desc  Fetch all products
+//* @route  GET /api/products
+//* @access  Public
 router.get("/", async (req, res) => {
-	const products = await Product.find({});
-	res.json(products);
+	try {
+		const products = await Product.find({});
+		res.json(products);
+	} catch (error) {
+		res.status(404).json({ message: "Products not found" });
+		console.log(error);
+	}
 });
 
-router.get("/:id", (req, res) => {
-	const product = products.find((p) => p._id === req.params.id);
-	res.json(product);
+//* @desc  Fetch single product
+//* @route  GET /api/products:id
+//* @access  Public
+router.get("/:id", async (req, res) => {
+	try {
+		const product = await Product.findById(req.params.id);
+		res.json(product);
+	} catch (error) {
+		res.status(404).json({ message: "Product not found" });
+		console.log(error);
+	}
 });
 
 module.exports = router;
