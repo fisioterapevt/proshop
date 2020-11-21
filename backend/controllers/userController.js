43666f122b968b3e841fc8362eabb85379e86ee9
Tikhon Izmailov <fisioterapevt@gmail.com>
2020-11-21 19:36:13 +0600

    added authauthorization on backend, token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/userModel");
+const generateToken = require("../utils/generateToken");
 
 //* @desc  Auth user & get token
 //* @route  POST /api/users/login
@@ -13,7 +14,7 @@ const authUser = async (req, res) => {
 				name: user.name,
 				email: user.email,
 				isAdmin: user.isAdmin,
-				token: null,
+				token: generateToken(user._id),
 			});
 		} else {
 			res.status(401).json({ message: "Invalid email or password" });
@@ -24,4 +25,54 @@ const authUser = async (req, res) => {
 	}
 };
 
-module.exports = { authUser };
+//* @desc  Register new user
+//* @route  POST /api/users
+//* @access  Public
+const registerUser = async (req, res) => {
+	try {
+		const { name, email, password } = req.body;
+
+		const userExists = await User.findOne({ email });
+
+		if (userExists) {
+			res.status(400).json({ message: "User alredy exists" });
+		}
+		const user = await User.create({ name, email, password });
+
+		if (user) {
+			res.status(201).json({
+				_id: user._id,
+				name: user.name,
+				email: user.email,
+				isAdmin: user.isAdmin,
+				token: generateToken(user._id),
+			});
+		} else {
+			res.status(400).json({ message: "Invalid user data" });
+		}
+	} catch (error) {
+		res.status(401).json({ message: error });
+		console.log(error);
+	}
+};
+
+//* @desc  Get user & get token
+//* @route  POST /api/users/profile
+//* @access  Private
+const getUserProfile = async (req, res) => {
+	try {
+		const user = await User.findById(req.user._id);
+
+		if (user) {
+			res.json({
+				_id: user._id,
+				name: user.name,
+				email: user.email,
+				isAdmin: user.isAdmin,
+			});
+		} else {
+			res.status(401).json({ message: "User not found" });
+		}
+	} catch (error) {}
+};
+module.exports = { authUser, registerUser, getUserProfile };
